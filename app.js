1e17b7d4a7f72a1405855a1f7f78045fa8d442e4
Mina Duranovic <mduranovic2@etf.unsa.ba>
2024-08-15 16:58:40 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require ('path');
+
+const db = require('./data/database');
+const authRoutes = require('./routes/auth.routes');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static('public'));
+
+app.use(authRoutes);
+
+db.connectToDatabase().then().catch(function(){
+    app.listen(3000);
+}).catch( function (error){
+    console.log("Failed to connect to the database");
+    console.log(error);
+});
+
+app.listen(3000);
