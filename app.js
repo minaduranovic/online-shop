42381fa37eb9e36f2cce994eda71fd389f28ae10
Mina Duranovic <mduranovic2@etf.unsa.ba>
2024-08-16 17:39:38 +0200

    config sessions

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-const express = require('express');
-const path = require ('path');
+const path = require('path');
 
+const express = require('express');
+const csrf = require('csurf');
+const expressSession = require('express-session');
+
+const createSessionConfig = require('./config/session');
 const db = require('./data/database');
+const addCsrfTokenMiddleware = require('./middlewares/csrf-token');
+const errorHandlerMiddleware = require('./middlewares/error-handler');
 const authRoutes = require('./routes/auth.routes');
 
 const app = express();
@@ -10,14 +16,24 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+
+const sessionConfig = createSessionConfig();
+
+app.use(expressSession(sessionConfig));
+app.use(csrf());
+
+app.use(addCsrfTokenMiddleware);
 
 app.use(authRoutes);
 
-db.connectToDatabase().then().catch(function(){
-    app.listen(3000);
-}).catch( function (error){
-    console.log("Failed to connect to the database");
-    console.log(error);
-});
+app.use(errorHandlerMiddleware);
 
-app.listen(3000);
+db.connectToDatabase()
+  .then(function () {
+    app.listen(3000);
+  })
+  .catch(function (error) {
+    console.log('Failed to connect to the database!');
+    console.log(error);
+  });
