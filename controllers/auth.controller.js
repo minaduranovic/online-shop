d5569a4eecf012f54706ba98e5b223b5d3b3b526
Mina Duranovic <mduranovic2@etf.unsa.ba>
2024-09-03 18:48:12 +0200

    error messages

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,13 +1,37 @@
-const { MongoNotConnectedError } = require("mongodb");
-const User = require("../models/user.model");
-const authUtil = require("../util/authentication");
-const validation = require("../util/validation");
+const User = require('../models/user.model');
+const authUtil = require('../util/authentication');
+const validation = require('../util/validation');
+const sessionFlash = require('../util/session-flash');
 
 function getSignup(req, res) {
-  res.render("customer/auth/signup");
+  let sessionData = sessionFlash.getSessionData(req);
+
+  if (!sessionData) {
+    sessionData = {
+      email: '',
+      confirmEmail: '',
+      password: '',
+      fullname: '',
+      street: '',
+      postal: '',
+      city: '',
+    };
+  }
+
+  res.render('customer/auth/signup', { inputData: sessionData });
 }
 
 async function signup(req, res, next) {
+  const enteredData = {
+    email: req.body.email,
+    confirmEmail: req.body['confirm-email'],
+    password: req.body.password,
+    fullname: req.body.fullname,
+    street: req.body.street,
+    postal: req.body.postal,
+    city: req.body.city,
+  };
+
   if (
     !validation.userDetailsAreValid(
       req.body.email,
@@ -17,11 +41,22 @@ async function signup(req, res, next) {
       req.body.postal,
       req.body.city
     ) ||
-    !validation.emailIsConfirmed(req.body.email, req.body["confirm-email"])
+    !validation.emailIsConfirmed(req.body.email, req.body['confirm-email'])
   ) {
-    res.redirect("/signup");
+    sessionFlash.flashDataToSession(
+      req,
+      {
+        errorMessage:
+          'Please check your input. Password must be at least 6 character slong, postal code must be 5 characters long.',
+        ...enteredData,
+      },
+      function () {
+        res.redirect('/signup');
+      }
+    );
     return;
   }
+
   const user = new User(
     req.body.email,
     req.body.password,
@@ -30,11 +65,21 @@ async function signup(req, res, next) {
     req.body.postal,
     req.body.city
   );
+
   try {
     const existsAlready = await user.existsAlready();
 
     if (existsAlready) {
-      res.redirect("/signup");
+      sessionFlash.flashDataToSession(
+        req,
+        {
+          errorMessage: 'User exists already! Try logging in instead!',
+          ...enteredData,
+        },
+        function () {
+          res.redirect('/signup');
+        }
+      );
       return;
     }
 
@@ -44,11 +89,20 @@ async function signup(req, res, next) {
     return;
   }
 
-  res.redirect("/login");
+  res.redirect('/login');
 }
 
 function getLogin(req, res) {
-  res.render("customer/auth/login");
+  let sessionData = sessionFlash.getSessionData(req);
+
+  if (!sessionData) {
+    sessionData = {
+      email: '',
+      password: '',
+    };
+  }
+
+  res.render('customer/auth/login', { inputData: sessionData });
 }
 
 async function login(req, res, next) {
@@ -60,8 +114,18 @@ async function login(req, res, next) {
     next(error);
     return;
   }
+
+  const sessionErrorData = {
+    errorMessage:
+      'Invalid credentials - please double-check your email and password!',
+    email: user.email,
+    password: user.password,
+  };
+
   if (!existingUser) {
-    res.redirect("/login");
+    sessionFlash.flashDataToSession(req, sessionErrorData, function () {
+      res.redirect('/login');
+    });
     return;
   }
 
@@ -70,18 +134,20 @@ async function login(req, res, next) {
   );
 
   if (!passwordIsCorrect) {
-    res.redirect("/login");
+    sessionFlash.flashDataToSession(req, sessionErrorData, function () {
+      res.redirect('/login');
+    });
     return;
   }
 
   authUtil.createUserSession(req, existingUser, function () {
-    res.redirect("/");
+    res.redirect('/');
   });
 }
 
 function logout(req, res) {
   authUtil.destroyUserAuthSession(req);
-  res.redirect("/login");
+  res.redirect('/login');
 }
 
 module.exports = {
--- util/validation.js
+++ util/validation.js
@@ -1,24 +1,28 @@
-function isEmpty() {
-  return !value || value.trim === "";
-}
-function userCredentialsAreValid(email, password){
-    return ( email &&
-    email.includes("@") &&
-    password.trim().length > 5)
-
-}
-function userDetailsAreValid(email, password, name, street, postal, city) {
-  return (
-    userCredentialsAreValid(email, password)&&
-    !isEmpty(name) &&
-    !isEmpty(street) &&
-    !isEmpty(postal) &&
-    !isEmpty(city)
-  );
-}
-function emailIsConfirmed(email, confirmEmail){
+function isEmpty(value) {
+    return !value || value.trim() === '';
+  }
+  
+  function userCredentialsAreValid(email, password) {
+    return (
+      email && email.includes('@') && password && password.trim().length >= 6
+    );
+  }
+  
+  function userDetailsAreValid(email, password, name, street, postal, city) {
+    return (
+      userCredentialsAreValid(email, password) &&
+      !isEmpty(name) &&
+      !isEmpty(street) &&
+      !isEmpty(postal) &&
+      !isEmpty(city)
+    );
+  }
+  
+  function emailIsConfirmed(email, confirmEmail) {
     return email === confirmEmail;
-}
-module.exports = {
+  }
+  
+  module.exports = {
     userDetailsAreValid: userDetailsAreValid,
-    emailIsConfirmed: emailIsConfirmed}
+    emailIsConfirmed: emailIsConfirmed,
+  };
