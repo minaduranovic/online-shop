729e6a7454c7f49b1278cc69f54cf25025fc6258
Mina Duranovic <mduranovic2@etf.unsa.ba>
2024-09-05 15:47:59 +0200

    mobile menu nav items

--- middlewares/check-auth.js
+++ middlewares/check-auth.js
@@ -1,13 +1,14 @@
 function checkAuthStatus(req, res, next) {
-    const uid = req.session.uid;
-  
-    if (!uid) {
-      return next();
-    }
-  
-    res.locals.uid = uid;
-    res.locals.isAuth = true;  
-    next();
+  const uid = req.session.uid;
+
+  if (!uid) {
+    return next();
   }
-  
-  module.exports = checkAuthStatus;
+
+  res.locals.uid = uid;
+  res.locals.isAuth = true; 
+  res.locals.isAdmin = req.session.isAdmin; 
+  next();
+}
+
+module.exports = checkAuthStatus;
