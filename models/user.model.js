e4efc116b49df535d673cd6e5153b68e90c311ce
Mina Duranovic <mduranovic2@etf.unsa.ba>
2024-10-12 21:48:39 +0200

    saving orders in database

--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs');
+const mongodb = require('mongodb');
 
 const db = require('../data/database');
 
@@ -14,12 +15,22 @@ class User {
     };
   }
 
+  static findById(userId) {
+    const uid = new mongodb.ObjectId(userId);
+
+    return db
+      .getDb()
+      .collection('users')
+      .findOne({ _id: uid }, { projection: { password: 0 } });
+  }
+
   getUserWithSameEmail() {
     return db.getDb().collection('users').findOne({ email: this.email });
   }
-  async existsAlready(){
+
+  async existsAlready() {
     const existingUser = await this.getUserWithSameEmail();
-    if (existingUser){
+    if (existingUser) {
       return true;
     }
     return false;
